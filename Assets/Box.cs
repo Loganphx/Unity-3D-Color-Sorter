7fa68291fdcfbd4ba696e9f2e90dc470c4890ca2
Loganphx <loganphx1@gmail.com>
2023-07-13 16:55:06 -0500

    - Added Feature to Change Ball Color when it enters a basket. - Setup UI Text to display each RGB value and displays the final hex code calculated from it

--- Assets/Box.cs
+++ Assets/Box.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    [SerializeField] public Color boxColor;
     public Action OnBallEntered;
     
     private void OnTriggerEnter(Collider other)
-    { 
+    {
+        other.GetComponent<Renderer>().material.color = boxColor;
         OnBallEntered?.Invoke();
     }
 }
--- Assets/MainManager.cs
+++ Assets/MainManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
@@ -9,16 +10,23 @@ public class MainManager : MonoBehaviour
     [SerializeField] private Box RedBox;
     [SerializeField] private Box GreenBox;
     [SerializeField] private Box BlueBox;
+    
+    [SerializeField] private Text _redText;
+    [SerializeField] private Text _greenText;
+    [SerializeField] private Text _blueText;
+    [SerializeField] private Text _colorText;
+    
     [SerializeField] private GameObject ballPreview;
 
+    
     [Header("Settings")]
     [SerializeField] private float moveAmount = 1f;
     
-    private int redScore;
-    private int greenScore;
-    private int blueScore;
+    [SerializeField] private int redScore;
+    [SerializeField] private int greenScore;
+    [SerializeField] private int blueScore;
     
-    private Color scoreColor;
+    [SerializeField] private Color scoreColor;
     
     public void Awake()
     {
@@ -89,8 +97,16 @@ public class MainManager : MonoBehaviour
     private void HandleScoreChanged()
     {
         float totalScore = redScore + greenScore + blueScore;
-        scoreColor = new Color(redScore / totalScore * 255, greenScore / totalScore * 255, blueScore / totalScore * 255);
-        Camera.main.backgroundColor = scoreColor;
+        var redHex = (int)Mathf.Round(redScore / totalScore * 255);
+        var greenHex = (int)Mathf.Round(greenScore / totalScore * 255);
+        var blueHex = (int)Mathf.Round(blueScore / totalScore * 255);
+        scoreColor = new Color(redHex, greenHex, blueHex);
 
+
+        _redText.text = $"R: {redHex}";
+        _greenText.text = $"G: {greenHex}";
+        _blueText.text = $"B: {blueHex}";
+        var hex = $"#{redHex:X2}{greenHex:X2}{blueHex:X2}";
+        _colorText.text = $"Hex: <color={hex}>{hex}</color>";
     }
 }
